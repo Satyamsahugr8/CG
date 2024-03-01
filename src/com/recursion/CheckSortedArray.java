39f157da86af174e21af0e0dbfe089af7a185398
satyamsahugr8 <satyamsahugr8@gmail.com>
2024-03-02 00:25:49 +0530

    recursion practice

---
+++ src/com/recursion/CheckSortedArray.java
@@ -0,0 +1,27 @@
+package com.recursion;
+
+public class CheckSortedArray {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        System.out.println(checkSorted(arr));
+    }
+
+    private static boolean checkSorted(int[] arr) {
+        return checkSorted(arr, 0);
+    }
+
+    private static boolean checkSorted(int[] arr, int si) {
+        if (arr[si] > arr[si+1]) {
+            return false;
+        }
+        if (si == arr.length-2) {
+            return true;
+        }
+
+        return checkSorted(arr, si+1);
+    }
+
+
+}
+
