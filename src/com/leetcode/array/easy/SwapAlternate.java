44236e8fd1b768f4d0d61be60ecaf7f19c987c06
satyam sahu <satyamsahugr8@gmail.com>
2024-08-11 20:12:17 +0530

    added SwapAlternate problem //[9,3,6,12,4,32] //[3,9,12,6,32,4]

---
+++ src/com/leetcode/array/easy/SwapAlternate.java
@@ -0,0 +1,25 @@
+package com.leetcode.array.easy;
+
+public class SwapAlternate {
+	
+	public static void swapAlternate(int[] arr) {
+		//[9,3,6,12,4,32]
+		for (int i = 0; i < arr.length; i = i+2) {
+			int temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+		}
+	}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {9,3,6,12,4,32};
+		swapAlternate(arr);
+		
+		for (int i : arr) {
+			System.out.println(i+" ");
+		}
+
+	}
+
+}
